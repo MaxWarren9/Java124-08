f74ae4e9682301e9682706632646a328e10fd1c2
MaxWarren9 <maximvoinoff@gmail.com>
2024-06-05 21:06:27 +0300

    1

---
+++ src/lab1/print.java
@@ -0,0 +1,15 @@
+package lab1;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
+public class print {
+    public static void main(String[] args) {
+
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java.");
+    }
+}
