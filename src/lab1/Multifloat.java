b9bef81a21da79fbe6000213d6e821698592445b
MaxWarren9 <maximvoinoff@gmail.com>
2024-06-07 01:28:37 +0300

    1

---
+++ src/lab1/Multifloat.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Multifloat {
+    public static void main(String[] args) {
+        double f1 = 3.6;
+        double f2 = 4.1;
+        double f3 = 5.9;
+        double result = f1 * f2 * f3;
+        System.out.println(result);
+    }
+}
